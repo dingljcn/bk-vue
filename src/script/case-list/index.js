20299c3fd2495e9e352f674cdcb18e845752e968
dingljcn <137146489+dingljcn@users.noreply.github.com>
2024-01-01 21:35:52 +0800

    Update index.js

--- src/script/case-list/index.js
+++ src/script/case-list/index.js
@@ -1,3 +1,5 @@
 import '../init.js';
 import './read-config.js';
 import './index.compile.min.js';
+
+alert(`请将【油猴脚本】《用例清单》中的 { https://dingljcn.github.io/bk-vue/ } 改为 { https://dingljcn.github.io/bk.script/ }, 即: 主要是把 'bk-vue' 改为 'bk.script'`);
